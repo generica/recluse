0288f2e35d3d1cc7c05f92b8d40a0039970137c2
brett <brett@8a9820b0-c509-4a4e-88fb-9abaf1767b83>
2008-10-23 03:27:59 +0000

    allow immediate

--- centos/5/autoupdates/runupdates.sh
+++ centos/5/autoupdates/runupdates.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-sleep $(( $RANDOM % 900 ))
+immediate=0
+
+if [ $# -eq 1 ]
+then
+	if [ $1 == "--immediate" ]
+	then
+		immediate=1
+	fi
+fi
+
+if [ $immediate -eq 0 ]
+then
+	sleep $(( $RANDOM % 900 ))
+fi
 
 for dir in $(find -mindepth 1 -maxdepth 1 -type d)
 do
